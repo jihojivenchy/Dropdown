dfd1c23a7ca4e4e9c8a0dc0f476c22846e4951b4
jihojivenchy <99619107+jihojivenchy@users.noreply.github.com>
2024-06-01 19:45:01 +0900

    feat: extension 접근자 처리

--- Sources/Dropdown/DropdownDelegate/DropdownDelegate.swift
+++ Sources/Dropdown/DropdownDelegate/DropdownDelegate.swift
@@ -8,12 +8,18 @@
 import Foundation
 
 public protocol DropdownDelegate: AnyObject {
+    /// The action to execute when the drop down will show.
     func willShow()
+    
+    /// The action to execute when the hides the drop down.
     func willHide()
+    
+    /// A closure that gets called when an item is selected in a list.
+    /// The closure takes two parameters: the selected element and the row of the selected cell
     func itemSelected(itemTitle: String, itemIndexRow: IndexRow)
 }
 
-extension DropdownDelegate {
+public extension DropdownDelegate {
     func willShow() { }
     func willHide() { }
     func itemSelected(itemTitle: String, itemIndexRow: IndexRow) { }
