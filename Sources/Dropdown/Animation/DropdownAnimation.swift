68e43d322d775a9bad379ebba4921ca316ec366c
jihojivenchy <99619107+jihojivenchy@users.noreply.github.com>
2024-06-04 01:53:37 +0900

    feat: downScaleTransform 조절 기능 추가

--- Sources/Dropdown/Animation/DropdownAnimation.swift
+++ Sources/Dropdown/Animation/DropdownAnimation.swift
@@ -11,6 +11,11 @@ public struct DropdownAnimation {
     let type: DropdownAnimationType
     let configuration: AnimationConfiguration
     
+    /// Initializes a new dropdown animation.
+    ///
+    /// - Parameters:
+    ///   - type: The type of animation. Defaults to `.slide`.
+    ///   - configuration: The configuration for the animation. If not provided, default values are used based on the animation type.
     public init(type: DropdownAnimationType = .slide, configuration: AnimationConfiguration? = nil) {
         self.type = type
         self.configuration = configuration ?? {
@@ -27,8 +32,35 @@ public enum DropdownAnimationType {
     case scale
 }
 
+/// A configuration for animation properties.
 public struct AnimationConfiguration {
     let duration: TimeInterval
     let damping: CGFloat
     let velocity: CGFloat
+    
+    /// The transform applied when the animation type is `.scale`.
+    /// This determines how much the view scales down before returning to its original size.
+    /// The default value is `CGAffineTransform(scaleX: 0.6, y: 0.6)`, meaning the view scales down to 60% of its original size.
+    let downScaleTransform: CGAffineTransform
+    
+    
+    /// Initializes a new animation configuration.
+    ///
+    /// - Parameters:
+    ///   - duration: The duration of the animation.
+    ///   - damping: The damping ratio for the animation.
+    ///   - velocity: The initial velocity of the animation.
+    ///   - downScaleTransform: The transform applied when the animation type is `.scale`.
+    ///     Default is `CGAffineTransform(scaleX: 0.6, y: 0.6)`.
+    public init(
+        duration: TimeInterval,
+        damping: CGFloat,
+        velocity: CGFloat,
+        downScaleTransform: CGAffineTransform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+    ) {
+        self.duration = duration
+        self.damping = damping
+        self.velocity = velocity
+        self.downScaleTransform = downScaleTransform
+    }
 }
