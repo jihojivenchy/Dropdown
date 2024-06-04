4496f8c18df1a2e16ea70b427bf22d27c75985f4
jihojivenchy <99619107+jihojivenchy@users.noreply.github.com>
2024-06-04 16:30:20 +0900

    feat: 비동기 처리

--- Sources/Dropdown/Dropdown.swift
+++ Sources/Dropdown/Dropdown.swift
@@ -562,15 +562,10 @@ extension Dropdown: UITableViewDelegate {
     }
     
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        DispatchQueue.main.async { [weak self] in
-//            self?.hide()
-//        }
-        
-        defer {
-            selectedItemIndexRow = indexPath.row
+        DispatchQueue.main.async { [weak self] in
+            self?.hide()
         }
-        
-        hide()
+        selectedItemIndexRow = indexPath.row
         delegate?.itemSelected(self, itemTitle: dataSource[indexPath.row], itemIndexRow: indexPath.row)
     }
 }
